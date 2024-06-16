0d607ce4006b5b45a058a382ebc1a6bf4957c3b8
Xinyaaaa <siowxinya99@gmail.com>
2024-06-16 21:36:18 +0800

    comment line 17,19,21

--- admin.php
+++ admin.php
@@ -16,9 +16,9 @@
    //fetch in array form 
    //$damages = mysqli_fetch_all($result, MYSQLI_ASSOC);
    
-   mysqli_free_result($result);
+   //mysqli_free_result($result);
 
-   mysqli_close($conn);
+   //mysqli_close($conn);
 
 ?>
 
