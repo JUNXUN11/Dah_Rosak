bd4cd04b3c7de799e88750bbcfe6926ee79ce4f2
Xinyaaaa <siowxinya99@gmail.com>
2024-06-17 16:54:53 +0800

    try

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -9,7 +9,7 @@ var myPieChart = new Chart(ctx, {
   data: {
     labels: locationLabels,
     datasets: [{
-      data: [55, 30, 15],
+      data: locationData,
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
