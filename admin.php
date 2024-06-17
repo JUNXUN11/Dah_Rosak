6d9075073811b5ad5d67f21508deabd15f3f4882
Xinyaaaa <siowxinya99@gmail.com>
2024-06-17 13:35:15 +0800

    dropdown

--- admin.php
+++ admin.php
@@ -268,8 +268,8 @@
                                         </a>
                                         <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
                                             aria-labelledby="dropdownMenuLink">
-                                            <div class="dropdown-header">Dropdown Header:</div>
-                                            <a class="dropdown-item" href="#">Action</a>
+                                            <div class="dropdown-header">For more:</div>
+                                            <a class="dropdown-item" href="tables.php">More Details</a>
                                             <a class="dropdown-item" href="#">Another action</a>
                                             <div class="dropdown-divider"></div>
                                             <a class="dropdown-item" href="#">Something else here</a>
