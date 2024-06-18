0ced2c7803ac2a984c69deecd8a5624da3a6027f
Xinyaaaa <siowxinya99@gmail.com>
2024-06-18 19:15:27 +0800

    aaa

--- admin.php
+++ admin.php
@@ -307,8 +307,7 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                                Unsolved Requests</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                Unsolved Requests</div class="h5 mb-0 font-weight-bold text-gray-800">
                                             <?php
                                                 echo $unsolved_reports;
                                             ?>
