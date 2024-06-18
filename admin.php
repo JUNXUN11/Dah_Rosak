538937e460faf7427cf1b4d18706a80adba103f9
Xinyaaaa <siowxinya99@gmail.com>
2024-06-18 19:17:31 +0800

    try

--- admin.php
+++ admin.php
@@ -305,12 +305,14 @@
                             <div class="card border-left-info shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                                Unsolved Requests</div class="h5 mb-0 font-weight-bold text-gray-800">
-                                            <?php
-                                                echo $unsolved_reports;
-                                            ?>
+                                    <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Unsolved Requests</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                <?php
+                                                    echo $unsolved_reports;
+                                                ?>
+                                            </div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-user fa-2x text-gray-300"></i>
