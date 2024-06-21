8e2d745f0498bba044df28dead35691db4f0f7fa
Jane Ng Jing Ying <janeng251@gmail.com>
2024-06-21 15:17:32 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -190,7 +190,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="tables.php">
                     <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
+                    <span>Reports</span></a>
             </li>
 
             <!-- Divider -->
